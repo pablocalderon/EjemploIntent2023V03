b24f2245bdb9c561bb8a991dc075751799cfb0fa
Pablo Calderon <pablocalderoncornejo@gmail.com>
2023-08-26 13:59:30 -0400

    V01 26/08/2023 se configuro el boton en el activiti uno e Intent

--- app/src/main/java/cl/isisur/ejemplointent2023v03/MainActivity.java
+++ app/src/main/java/cl/isisur/ejemplointent2023v03/MainActivity.java
@@ -2,13 +2,35 @@ package cl.isisur.ejemplointent2023v03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton;
+    EditText etnombre,etmatricula;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etnombre=(EditText) findViewById(R.id.etNombre);
+        etmatricula=(EditText) findViewById(R.id.etMatricula);
+        intent= new Intent(this, MainActivity2.class);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
